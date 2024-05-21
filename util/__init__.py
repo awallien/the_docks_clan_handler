0253462b7b80c8e37e04cb4b8cfcdf65d01f060e
awallien <alexswall2016@gmail.com>
2024-05-20 23:28:45 -0700

    init

--- util/__init__.py
+++ util/__init__.py
@@ -1,3 +1,4 @@
 from .logger import debug_set_enable, debug_print, err_print
 from .prompt_args import PromptArgsResponse, RESPONSE_ERR, RESPONSE_OK, PromptArgs, PromptRunner, default_response_ok
 from .player_rank import PlayerRankHandler, sanitize_player_rank
+from .osrs_api import *
---
+++ util/osrs_api/__init__.py
@@ -0,0 +1,4 @@
+from .skills import Skills, Skill, SKILLS
+from .activities import Activities, Activity, ACTIVITIES
+from .bosses import Bosses, Boss, BOSSES
+from .hiscore import Hiscore, AccountTypes
