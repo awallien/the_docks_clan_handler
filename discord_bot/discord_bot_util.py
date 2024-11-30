53115109276843f7a81df904b849e72493ad4245
Alex Wall <33971269+awallien@users.noreply.github.com>
2024-11-30 06:21:47 -0800

    Clan update - November 2024 (#33)
    * Clan Update 11-8-2024 and 11-18-2024
    
    * 11-28-2024 clan update

--- discord_bot/discord_bot_util.py
+++ discord_bot/discord_bot_util.py
@@ -4,7 +4,7 @@ ICON_URI_PATH = "https://oldschool.runescape.wiki/images/Clan_icon_-_"
 rank_to_icon = {
     '1':"Gnome_Child.png?b0561",    '2':"Kitten.png?9dc78",
     '3':"Adventurer.png?3630c",     '4':"Crew.png?6c963",
-    '5':"Adventurer.png?3630c",     '6':"Fire.png?f7cb3",
+    '5':"Achiever.png?45aef",       '6':"Fire.png?f7cb3",
     '7':"Inquisitor.png?0f3a8",     '8':"Barbarian.png?f92d8",
     '9':"Diamond.png?f7cb3",        '10':"Crusader.png?87d1d",
     '11':"Beast.png?53696",         '12':"Epic.png?f3acc",
